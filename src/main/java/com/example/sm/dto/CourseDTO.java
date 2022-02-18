3a1ff6136de7c1c45924b4548019ba7c7457e925
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-02-18 12:03:36 +0330

    Update CourseDTO.java

--- src/main/java/com/example/sm/dto/CourseDTO.java
+++ src/main/java/com/example/sm/dto/CourseDTO.java
@@ -2,6 +2,8 @@ package com.example.sm.dto;
 
 import com.example.sm.model.Faculty;
 import com.example.sm.model.Professor;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,48 +12,24 @@ import javax.validation.constraints.Size;
 
 public class CourseDTO {
 
+	@Getter
+	@Setter
 	@NotEmpty(message = "name must not be empty")
 	@Size(min = 2, max = 15)
 	private String name;
 
+	@Getter
+	@Setter
 	@Range(min = 1, max = 5, message = "invalid unit")
 	private int unit;
 
+	@Getter
+	@Setter
 	@NotNull(message = "professor cannot be null")
 	private Professor professor;
 
+	@Getter
+	@Setter
 	@NotNull(message = "faculty cannot be null")
 	private Faculty faculty;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getUnit() {
-		return unit;
-	}
-
-	public void setUnit(int unit) {
-		this.unit = unit;
-	}
-
-	public Professor getProfessor() {
-		return professor;
-	}
-
-	public void setProfessor(Professor professor) {
-		this.professor = professor;
-	}
-
-	public Faculty getFaculty() {
-		return faculty;
-	}
-
-	public void setFaculty(Faculty faculty) {
-		this.faculty = faculty;
-	}
 }
