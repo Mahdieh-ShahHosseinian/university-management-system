2ad3fa947224a1a3721955d7c829dea585fbfb3e
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-02-18 12:00:30 +0330

    Update Student.java

--- src/main/java/com/example/sm/model/Student.java
+++ src/main/java/com/example/sm/model/Student.java
@@ -1,35 +1,53 @@
 package com.example.sm.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "studentCourses"})
-public class Student extends RepresentationModel<Student> {
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "studentCourses", "role", "password"})
+public class Student extends User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    @Getter
+    @Setter
     @Column(unique = true, nullable = false)
     private int studentId;
 
+    @Getter
+    @Setter
     @Column(unique = true, nullable = false, length = 10)
     private long nationalId;
 
+    @Getter
+    @Setter
     @Column(nullable = false, length = 20)
     private String firstName;
 
+    @Getter
+    @Setter
     @Column(nullable = false, length = 20)
     private String lastName;
 
+    @Getter
+    @Setter
     @OneToMany(mappedBy = "studentCourseId.student", fetch = FetchType.EAGER)
     private Set<StudentCourse> studentCourses;
 
+    @Getter
+    @Setter
     @ManyToOne
     private Faculty faculty;
+
+
+    public Student() {
+        role = "STUDENT";
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -38,55 +56,5 @@ public class Student extends RepresentationModel<Student> {
         if (obj instanceof Student) student = (Student) obj;
         assert student != null;
         return studentId == student.getStudentId();
-    }
-
-    // Setter & Getter
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public long getNationalId() {
-        return nationalId;
-    }
-
-    public void setNationalId(long nationalId) {
-        this.nationalId = nationalId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Set<StudentCourse> getStudentCourses() {
-        return studentCourses;
-    }
-
-    public void setStudentCourses(Set<StudentCourse> studentCourses) {
-        this.studentCourses = studentCourses;
-    }
-
-    public Faculty getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(Faculty faculty) {
-        this.faculty = faculty;
     }
 }
