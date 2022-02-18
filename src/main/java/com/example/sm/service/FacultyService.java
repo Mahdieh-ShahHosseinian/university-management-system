44cbe70b8d1648f284bea9b276fd00aa594cd4e0
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-02-18 11:57:08 +0330

    Update FacultyService.java

--- src/main/java/com/example/sm/service/FacultyService.java
+++ src/main/java/com/example/sm/service/FacultyService.java
@@ -2,17 +2,17 @@ package com.example.sm.service;
 
 import com.example.sm.dao.FacultyRepository;
 import com.example.sm.model.Faculty;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class FacultyService implements ServiceInterface<Faculty> {
 
-    @Autowired
     private FacultyRepository repository;
-
 
     @Override
     public Faculty save(Faculty faculty) {
