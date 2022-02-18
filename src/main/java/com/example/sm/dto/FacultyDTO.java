daf2efad3594498e3b31bf3651a41ed8a83359fa
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-02-18 12:03:50 +0330

    Update FacultyDTO.java

--- src/main/java/com/example/sm/dto/FacultyDTO.java
+++ src/main/java/com/example/sm/dto/FacultyDTO.java
@@ -1,19 +1,16 @@
 package com.example.sm.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class FacultyDTO {
 
+	@Getter
+	@Setter
 	@NotEmpty(message = "name must not be empty")
 	@Size(min = 2, max = 15)
 	private String name;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
