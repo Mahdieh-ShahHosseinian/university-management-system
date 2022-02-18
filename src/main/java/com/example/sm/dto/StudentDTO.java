8b6a8d925272b9df5e7f5b3e258dd569556785b7
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-02-18 12:04:32 +0330

    Update StudentDTO.java

--- src/main/java/com/example/sm/dto/StudentDTO.java
+++ src/main/java/com/example/sm/dto/StudentDTO.java
@@ -1,55 +1,45 @@
 package com.example.sm.dto;
 
+import com.example.sm.model.Faculty;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.example.sm.model.Faculty;
-
 public class StudentDTO {
-	
-	private long nationalId;
 
-	@NotEmpty(message = "firstName must not be empty")
-	@Size(min = 2, max = 15)
-	private String firstName;
+    @Getter
+    @Setter
+    private long nationalId;
 
-	@NotEmpty(message = "lastName must not be empty")
-	@Size(min = 2, max = 15)
-	private String lastName;
+    @Getter
+    @Setter
+    @NotEmpty(message = "firstName must not be empty")
+    @Size(min = 2, max = 15)
+    private String firstName;
 
-	@NotNull(message = "faculty cannot be null")
-	private Faculty faculty;
+    @Getter
+    @Setter
+    @NotEmpty(message = "lastName must not be empty")
+    @Size(min = 2, max = 15)
+    private String lastName;
 
-	public long getNationalId() {
-		return nationalId;
-	}
+    @Getter
+    @Setter
+    @NotEmpty(message = "firstName must not be empty")
+    @Size(min = 4, max = 15)
+    private String username;
 
-	public void setNationalId(long nationalId) {
-		this.nationalId = nationalId;
-	}
+    @Getter
+    @Setter
+    @NotEmpty(message = "firstName must not be empty")
+    @Size(min = 6, max = 15)
+    private String password;
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Faculty getFaculty() {
-		return faculty;
-	}
-
-	public void setFaculty(Faculty faculty) {
-		this.faculty = faculty;
-	}
+    @Getter
+    @Setter
+    @NotNull(message = "faculty cannot be null")
+    private Faculty faculty;
 }
