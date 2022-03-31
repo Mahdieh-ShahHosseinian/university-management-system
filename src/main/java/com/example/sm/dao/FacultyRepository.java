89f12bb33206fd263465a57faf810aed2881b35f
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-03-31 17:49:09 +0430

    Delete src directory

--- src/main/java/com/example/sm/dao/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sm.dao;
-
-import com.example.sm.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
-}
