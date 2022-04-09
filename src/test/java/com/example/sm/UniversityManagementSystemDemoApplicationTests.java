868118da08c5c4960910b15505c9d2f392b4ea95
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-04-09 14:11:05 +0430

    Add files via upload
    unit test on StudentCRUDService

---
+++ src/test/java/com/example/sm/UniversityManagementSystemDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+class UniversityManagementSystemDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
