52d5bb8864c853bfad066c12f0bc9fd753833d9c
Mahdieh-ShahHosseinian <87202825+Mahdieh-ShahHosseinian@users.noreply.github.com>
2022-04-13 22:12:19 +0430

    Update APIController.java

--- src/main/java/com/example/sm/controller/APIController.java
+++ src/main/java/com/example/sm/controller/APIController.java
@@ -2,5 +2,5 @@ package com.example.sm.controller;
 
 public class APIController {
 
-    public static final String BASE_URI = "/ums.com/api/v5/";
+    public static final String BASE_URI = "/ums.com/api/v6/";
 }
